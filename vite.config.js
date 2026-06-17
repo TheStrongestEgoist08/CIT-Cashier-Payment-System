ade4730b365b95a5510825b34f56819979ecc3b5
TheStrongestEgoist08 <bigalcarlmichael799@gmail.com>
2026-06-17 16:00:00 +0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,21 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+
+        // Strong CORS fix for other devices
+        cors: {
+            origin: ['http://192.168.68.189:8000', 'http://localhost:8000'],
+            methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            credentials: true,
+        },
+
+        hmr: {
+            host: '192.168.68.189',
+            protocol: 'http',
+        },
+    },
 });
